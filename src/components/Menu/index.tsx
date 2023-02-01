d35097ecb534b0eb22cd02460ced3c81b2b37efd
Renildo Paes <renildopaesjoob@gmail.com>
2023-01-31 22:46:09 -0300

    modal ingredientes

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,51 +1,70 @@
+import { useState } from "react";
 import { FlatList } from "react-native";
 import { products } from "../../mocks/products";
 import { formatCurrency } from "../../utils/formatCurrency";
 import { PlusCircle } from "../Icons/PlusCircle";
+import { PorductModal } from "../ProductModal";
 import { Text } from "../Text";
+import { Product } from "../types/Product";
 import {
 	AddToCardButton,
-	Product,
+	ProductContainer,
 	ProductDetails,
 	ProductImage,
 	Separator,
 } from "./styles";
 
 export function Menu() {
+
+	const [isProductModalVisible, setProductModalVisible] = useState(false)
+	const [selectProduct, setSelectProduct] = useState<null | Product>(null)
+
+	function handleOpenModal(product: Product){
+		setProductModalVisible(true)
+		setSelectProduct(product)
+	}
+
 	return (
-		<FlatList
-			style={{ marginTop: 32 }}
-			contentContainerStyle={{ paddingHorizontal: 24 }}
-			data={products}
-			keyExtractor={(product) => product._id}
-			ItemSeparatorComponent={Separator}
-			renderItem={({ item: product }) => {
-				return (
-					<Product>
-						<ProductImage
-							source={{
-								uri: `http://192.168.1.16:3001/uploads/${product.imagePath}`,
-							}}
-						/>
+		<>
+			<PorductModal
+				visible={isProductModalVisible}
+				onClose={() => setProductModalVisible(false)}
+				product={selectProduct}
+			/>
+			<FlatList
+				style={{ marginTop: 32 }}
+				contentContainerStyle={{ paddingHorizontal: 24 }}
+				data={products}
+				keyExtractor={(product) => product._id}
+				ItemSeparatorComponent={Separator}
+				renderItem={({ item: product }) => {
+					return (
+						<ProductContainer onPress={() => handleOpenModal(product)}>
+							<ProductImage
+								source={{
+									uri: `http://192.168.1.16:3001/uploads/${product.imagePath}`,
+								}}
+							/>
 
-						<ProductDetails>
-							<Text weight="600">{product.name}</Text>
+							<ProductDetails>
+								<Text weight="600">{product.name}</Text>
 
-							<Text size={14} color="#667">
-								{product.description}
-							</Text>
+								<Text size={14} color="#667">
+									{product.description}
+								</Text>
 
-							<Text size={14} weight="600" style={{ marginVertical: 8 }}>
-								{formatCurrency(product.price)}
-							</Text>
-						</ProductDetails>
+								<Text size={14} weight="600" style={{ marginVertical: 8 }}>
+									{formatCurrency(product.price)}
+								</Text>
+							</ProductDetails>
 
-						<AddToCardButton>
-							<PlusCircle />
-						</AddToCardButton>
-					</Product>
-				);
-			}}
-		/>
+							<AddToCardButton>
+								<PlusCircle />
+							</AddToCardButton>
+						</ProductContainer>
+					);
+				}}
+			/>
+		</>
 	);
 }
