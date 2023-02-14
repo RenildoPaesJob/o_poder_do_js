baee9efc4d5da619a7cfb965a99fb68b556fa357
Renildo Paes <renildopaesjoob@gmail.com>
2023-02-14 08:35:26 -0300

    back-end finalizado usando websocket

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,27 @@
 import path from "node:path"
+import http from "node:http"
 import express from "express"
 import mongoose from "mongoose"
+import { Server } from "socket.io"
 
 import { router } from "./router"
+
+const app    = express()
+//web socket connection
+const server = http.createServer(app)
+export const io     = new Server(server)
+
+io.on("connect", () => {
+	console.log("Conectou ao Socket")
+})
 
 mongoose.set("strictQuery", true)
 
 mongoose.connect("mongodb://localhost:27017")
 	.then(() => {
 		console.log("Conectado ao MongoDB")
-		const app = express()
+		const port   = 3001
+
 
 		app.use((req, res, next) => {
 			res.setHeader("Access-Control-Allow-Origin", "*")
@@ -22,8 +34,8 @@ mongoose.connect("mongodb://localhost:27017")
 		app.use(express.json())
 		app.use(router)
 
-		const port = 3001
-		app.listen(port, () => {
+		//listen web socket connection
+		server.listen(port, () => {
 			console.log(`🚀 Server is running on http://localhost:${port} 🔥`)
 		})
 	})
