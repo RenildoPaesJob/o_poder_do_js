65c6f5f0de15cb0bc0743d3cc99b35ca14d07c8b
Renildo Paes <renildopaesjoob@gmail.com>
2023-02-14 08:42:04 -0300

    mobile

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const portServer = 3001
-const urlServer  = 'http://192.168.1.16'
+const urlServer  = 'http://192.168.1.11'
 
 export const api = axios.create({
 	baseURL: `${urlServer}:${portServer}`
